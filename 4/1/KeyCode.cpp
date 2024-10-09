c2cfd412a4d01717a55c7ec86fa90685936584e5
JpPamplona <jp.alp2014@gmail.com>
2024-10-09 13:36:27 -0300

    Finalizado Código + Hints - Soma de Elementos Consecutivos (Guloso)

--- 4/1/KeyCode.cpp
+++ 4/1/KeyCode.cpp
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <algorithm>
 
 using namespace std;
 
-long long int SelAtividades(long long int * A, int n){
-	long long int sMax = A[0]; //Maior soma de elementos contíguos considerando somente os primeiros i-1 elementos de A
-	long long int sI = A[0];   //Maior soma de elementos contíguos que terminam em i
+int MaxSubsum(int * A, int n){
+	// codev
+	int sMax = A[1]; //Maior soma de elementos contíguos considerando somente os primeiros i-1 elementos de A
+	int sMaxT = A[1];   //Maior soma de elementos contíguos que terminam em i
 	
-	for (int i = 1; i < n; i++){
-		sI = max(sI + A[i],A[i]);
-		sMax = max(sMax, sI);
+	for (int i = 2; i <= n; i++){
+		sMaxT = max(sMaxT + A[i], A[i]);
+		sMax = max(sMax, sMaxT);
 	}
 	
 	return sMax;
+	// codev
 }
-
 
 int main() {
 	int n; 
-	long long int * A; 
+	int * A; 
 	while (scanf("%d", &n)>0) {
-		if (n>=0) { 
-			A = (long long int *) malloc(sizeof(long long int) * n);
-			for(int i = 0; i < n; i++){
-				scanf("%d", &A[i]);
-			} 
-			printf("%d\n", SelAtividades(A, n));
+		bool testeDeCarga = n == -1;
+		if (testeDeCarga) {
+			n = 1000000;
 		}
+		A = (int *) malloc(sizeof(int) * n);
+		for(int i = 1; i <= n; i++){
+			if (testeDeCarga) {
+				A[i] = i % 5 == 0 ? 5 : -1;
+			} else {
+				scanf("%d", &A[i]);
+			}
+		} 
+		printf("%d\n", MaxSubsum(A, n));
+		free(A);
 	}
-	free(A);
+	return 0;
 }
