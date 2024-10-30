3310b9cb58d43b1f37c5a9548e526c04d7d6b5dd
JpPamplona <jp.alp2014@gmail.com>
2024-10-30 11:57:01 -0300

    Finalizado Código + Hints - Arranjos (Backtracking)

---
+++ 2/3/KeyCode.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Assume:  n >= k, 0 <= d <= k
+Garante: Escrita de todas as permutações de {1,...,n} de tamanho k
+         que contenha os d elementos b[0..d-1]
+*/
+void Arranjo(int n, int k, int b[], int d){
+    if (k == d) {
+    	printf("(");
+        for (int i = 1; i <= k; i++){
+            printf("%d", b[i]);
+            if(i != k){
+              printf(",");
+            }
+        }
+
+        printf(")");
+    } else {
+        for (int i = 1; i <= n; i++){
+            bool enc = false;
+            for (int j = 1; j <= d; j++){ 
+                if(b[j] == i){
+                    enc = true; 
+                    break;
+                }
+            }
+            if(!enc){
+                b[d+1] = i;
+                Arranjo(n, k, b, d+1);
+            }
+        } 
+    }
+}
+
+int main() {
+	int n, k;
+	while (scanf("%d %d", &n, &k)>0) {
+        int* b = (int *) malloc(sizeof(int) * (k+1));
+        Arranjo(n, k, b, 0);
+        printf("\n");
+	}
+}
